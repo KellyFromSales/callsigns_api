e1aea66883b593895375148a44376b2d70172119
KellyFromSales <kellyfromsales@gmail.com>
2022-04-30 15:05:03 +0100

    formatting improvements

--- db/callsigns.js
+++ db/callsigns.js
@@ -1,34 +1,34 @@
 const knex = require("./knex");
 
 function addCallsign(cs) {
-    return knex("callsigns").insert(cs).onConflict('Value').ignore();
+  return knex("callsigns").insert(cs).onConflict("Value").ignore();
 }
 
 function getAllCallsigns() {
-    return knex("callsigns").select("*");
+  return knex("callsigns").select("*");
 }
 
 function getCallsign(id) {
-    return knex("callsigns").where("Value", id);
+  return knex("callsigns").where("Value", id);
 }
 
 function getCallsignByLevel(level) {
-    return knex("callsigns").where("Level", level);
+  return knex("callsigns").where("Level", level);
 }
 
 function deleteCallsign(id) {
-    return knex("callsigns").where("Value", id).del();
+  return knex("callsigns").where("Value", id).del();
 }
 
 function updateCallsign(id, cs) {
-    return knex("callsigns").where("Value", id).update(cs);
+  return knex("callsigns").where("Value", id).update(cs);
 }
 
 module.exports = {
-    addCallsign,
-    getAllCallsigns,
-    deleteCallsign,
-    updateCallsign,
-    getCallsign,
-    getCallsignByLevel
-}
+  addCallsign,
+  getAllCallsigns,
+  deleteCallsign,
+  updateCallsign,
+  getCallsign,
+  getCallsignByLevel,
+};
--- server.js
+++ server.js
@@ -3,37 +3,37 @@ const app = express();
 const bodyParser = require("body-parser");
 const db = require("./db/callsigns");
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post("/callsigns", async (req,res) =>{
-    const results = await db.addCallsign(req.body);
-    res.status(201).json({ id: results[0] });
-})
+app.post("/callsigns", async (req, res) => {
+  const results = await db.addCallsign(req.body);
+  res.status(201).json({ id: results[0] });
+});
 
-app.get("/callsigns", async (req,res) =>{
-    const Callsigns = await db.getAllCallsigns();
-    res.status(200).json({ Callsigns });
-})
+app.get("/callsigns", async (req, res) => {
+  const Callsigns = await db.getAllCallsigns();
+  res.status(200).json({ Callsigns });
+});
 
-app.get("/callsigns/:id", async (req,res) =>{
-    const Callsign = await db.getCallsign(req.params.id);
-    res.status(200).json({ Callsign });
-})
+app.get("/callsigns/:id", async (req, res) => {
+  const Callsign = await db.getCallsign(req.params.id);
+  res.status(200).json({ Callsign });
+});
 
-app.get("/callsigns/level/:level", async (req,res) =>{
-    const Callsigns = await db.getCallsignByLevel(req.params.level);
-    res.status(200).json({ Callsigns });
-})
+app.get("/callsigns/level/:level", async (req, res) => {
+  const Callsigns = await db.getCallsignByLevel(req.params.level);
+  res.status(200).json({ Callsigns });
+});
 
-app.patch("/callsigns/:id", async (req,res) =>{
-    const Callsign = await db.updateCallsign(req.params.id, req.body);
-    res.status(200).json({ Callsign });
-})
+app.patch("/callsigns/:id", async (req, res) => {
+  const Callsign = await db.updateCallsign(req.params.id, req.body);
+  res.status(200).json({ Callsign });
+});
 
-app.delete("/callsigns/:id", async (req,res) =>{
-    await db.deleteCallsign(req.params.id);
-    res.status(200).json({ sucess: true });
-})
+app.delete("/callsigns/:id", async (req, res) => {
+  await db.deleteCallsign(req.params.id);
+  res.status(200).json({ sucess: true });
+});
 
 app.listen(5001, () => console.log("server is listening on port 5001"));
